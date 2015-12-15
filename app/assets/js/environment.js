698373b641e2d274283361411d133920303e3086
Arafat <arafato@acbc32c48889.ant.amazon.com>
2015-12-15 17:05:04 +0100

    restructuring of routes, added environment object

---
+++ app/assets/js/environment.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var Environment = (function (e) {
+
+  e.FEATURES_SERVICE = 'https://1gn0ds8mh4.execute-api.eu-west-1.amazonaws.com/development/api/features';
+  e.KEYWORD_SERVICE = 'https://1gn0ds8mh4.execute-api.eu-west-1.amazonaws.com/development/api/keyword';
+  e.SLIDES_SERVICE = 'https://1gn0ds8mh4.execute-api.eu-west-1.amazonaws.com/development/api/slides';
+
+  return e;
+
+} (Environment || {}));
--- app/assets/js/models/sammyViewModel.js
+++ app/assets/js/models/sammyViewModel.js
@@ -2,27 +2,37 @@
 
 /* global define:true*/
 define(['jquery',
-    'knockout',
-    'sammy'
-    ], function ($, ko, Sammy) {
+  'knockout',
+  'sammy'
+], function ($, ko, Sammy) {
   return function () {
     var self = this;
 
     self.chosenFolderId = ko.observable();
 
     self.goToFolder = function (folder) { self.chosenFolderId(folder); };
-    
+
     self._sammy = new Sammy(function () {
-    
-      this.get('#:folder', function () {
-        self.chosenFolderId(this.params.folder);
+
+      this.get('#home/:service', function () {
+        self.chosenFolderId('home');
+        console.log(this.params.service);
+        console.log((!this.params.startdate) ? 'no params defined' : this.params.startdate);
+      });
+
+      this.get('#api', function () {
+        self.chosenFolderId('api');
+      });
+      
+      this.get('#contact', function () {
+        self.chosenFolderId('contact');
       });
 
       this.get('', function () {
-        this.app.runRoute('get', '#home');
+        this.redirect('#home');
       });
     });
-    
+
     self._sammy.run();
   };
 });
